672942e4b63db39006a245bf581f5452f50b4d71
yatosz <--global>
2024-07-01 17:25:19 -0300

    my first application-Python

---
+++ Alura-Python/app.py
@@ -0,0 +1,14 @@
+print("""Sabor Express
+      
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+      
+      """)
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+escolha_opcao = input("Escolha uma opção: ")
+print(f"Você escolheu a opção {escolha_opcao}")
