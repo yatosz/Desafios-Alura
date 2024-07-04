c30958cb935ad12f58d65b7415478d814aac21a3
yatosz <--global>
2024-07-04 13:19:31 -0300

    ajustando caso de opcao invalida

--- Alura-Python/app.py
+++ Alura-Python/app.py
@@ -16,31 +16,42 @@ def mostra_opcoes():
       print("3. Ativar restaurante")
       print("4. Sair\n")
 
+
+def opcao_invalida():
+      print("\nOpção inválida!\n")
+      input("Digite uma tecla para voltar ao menu principal: ")
+      main()
+
 def finalizar_app():
     print('Finalizando o app...\n')
 
 def escolhe_opcao():
-      escolha_opcao = int(input("Escolha uma opção: "))
-      #  escolha_opcao = int(escolha_opcao)
-      match escolha_opcao:
-            case 1:
-                  print("Cadastrar restaurante")
+      try:
+            escolha_opcao = int(input("Escolha uma opção: "))
+            #  escolha_opcao = int(escolha_opcao)
+            match escolha_opcao:
+                  case 1:
+                        print("Cadastrar restaurante")
 
-            case 2:
-                  print("Cadastrar restaurantes")
+                  case 2:
+                        print("Listar restaurantes")
 
-            case 3:
-                  print("Ativar restaurante")
+                  case 3:
+                        print("Ativar restaurante")
 
-            case 4:
+                  case 4:
                   #os.system("cls") windows
-                  os.system("clear") # mac / linux
-                  finalizar_app()
-      
-            case _:
-                  print("\nOpção inválida!\n")
+                        os.system("clear") # mac / linux
+                        finalizar_app()
+
+                  case _:
+                        opcao_invalida()
+            
+      except:
+             opcao_invalida()
 
 def main():
+    os.system("clear")
     nome_programa()
     mostra_opcoes()
     escolhe_opcao()
