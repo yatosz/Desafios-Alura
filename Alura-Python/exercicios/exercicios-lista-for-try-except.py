e1ce76f14cd83691871d8608cd086663fd0a1670
igor.eufrasio <--global>
2024-09-05 09:39:16 -0300

    novos exercicios

---
+++ Alura-Python/exercicios/exercicios-lista-for-try-except.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+# 1 - Crie uma lista para cada informação a seguir:
+
+lista_numeros = ['1', '2']
+
+def listar_numeros():
+    for lista in lista_numeros:
+        print(f'.{lista}\n')
+    
+
