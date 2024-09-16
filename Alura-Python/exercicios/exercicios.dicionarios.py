5bb1d24556301b0a8c84b8133b43d6a5726c595a
igor.eufrasio <--global>
2024-09-16 10:26:25 -0300

    novos exercicios

---
+++ Alura-Python/exercicios/exercicios.dicionarios.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+informacoes_pessoas = [{'nome':'Igor Vidal', 'idade':'24', 'Cidade':'DV'},
+                       {'nome': 'Hanna', 'idade':'24', 'Cidade':'DV'},
+                       {'nome': 'Nancy', 'idade': '2', 'Cidade':'Toledo'}]
+
+print(informacoes_pessoas)
+
