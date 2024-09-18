5c662e968ca49fadf255a8b4a6fc54167675d0dc
igor.eufrasio <--global>
2024-09-18 14:15:16 -0300

    Primeiro Desafio JS | Alura

--- Lógica de Programção/app.js
+++ Lógica de Programção/app.js
@@ -1,10 +1,28 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+titulo.innerHTML = 'Hora do Desafio';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function verificarChute(){
-    console.log('O botão foi clicado!')
+function exibirNoConsole(){
+    console.log('O botão foi clicado!');
 
+}
+
+function exibirAlerta(){
+    alert('Eu Amo JS');
+}
+
+function exibirPrompt(){
+    let nomeCidade = prompt('Digite o nome de uma cidade do Brasil');
+    alert(`Estive em ${nomeCidade} e lembrei de você`);
+}
+
+function somaResultado(){
+    let primeiroNumero = parseInt(prompt('Informe o primeiro número: '));
+    let segundoNumero = parseInt(prompt('Informe o segundo número: '));
+
+    let resultadoSoma = primeiroNumero + segundoNumero;
+
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultadoSoma}`);
 }
