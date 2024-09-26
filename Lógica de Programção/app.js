3243cec59ae3684786d17ac6569f3a0fed540d30
igor.eufrasio <--global>
2024-09-26 16:01:29 -0300

    Adicionando novas funcoes com parametros | Alura

--- Lógica de Programção/app.js
+++ Lógica de Programção/app.js
@@ -4,7 +4,15 @@ titulo.innerHTML = 'Jogo do número secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+function exibirTextoTela(tag, texto){
+let campo = document.querySelector(tag);
+campo.innerHTML = texto;
+}
+
+exibirTextoTela('h1', 'Jogo do número secreto')
+exibirTextoTela('p', 'Escolha um númer  o entre 1 e 10')
+
 function verificarChute(){
-    console.log('O botão foi clicado!')
+    console.log('O botão foi clicado!');
 
 }
